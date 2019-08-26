0e9395ec1ecc0f7432337dcdc98dbc603a227799
yang <251814590@163.com>
2019-08-26 11:02:53 +0800

    Initial commit

---
+++ src/main/java/MD5AutoConfiguration.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MD5AutoConfiguration {
+    @Bean
+    MD5Service md5Service() {
+        return new MD5Service();
+    }
+}
---
+++ src/main/java/MD5Service.java
@@ -0,0 +1,5 @@
+public class MD5Service {
+    public String getMD5( String input ) {
+        return MD5Util.getMD5( input.getBytes() );
+    }
+}
